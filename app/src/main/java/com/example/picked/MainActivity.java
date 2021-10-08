91f1ce4269c2659666cade0ffec9746bd4b1c49d
tanner210 <tanner.210@osu.edu>
2021-10-08 18:38:14 -0400

    main screen

--- app/src/main/java/com/example/picked/MainActivity.java
+++ app/src/main/java/com/example/picked/MainActivity.java
@@ -1,14 +1,17 @@
 package com.example.picked;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import timber.log.Timber;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        if (BuildConfig.DEBUG) {
+            Timber.plant(new Timber.DebugTree());
+        }
     }
 }
